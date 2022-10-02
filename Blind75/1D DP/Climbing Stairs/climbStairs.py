afd0c713d06987878946c6074eafe88c0558b4f5
Jun Ham <hamjun7@gmail.com>
2022-10-01 23:34:08 -0600

    add Climbing Stairs

---
+++ Blind75/1D DP/Climbing Stairs/climbStairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        one, two = 1,1
+        
+        for i in range(n-1):
+            temp = one
+            one += two
+            two = temp
+        return one
+    
+if __name__ == "__main__":
+    n = 5
+    obj = Solution()
+    print(obj.climbStairs(n))
